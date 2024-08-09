590e586ae8140e4e22ae263b996c6fcc632a9cd3
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2024-08-09 12:04:35 +0300

    save changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const name = "Марія Калініченко";
+const imageUrl = 'https://i.pinimg.com/736x/05/5e/84/055e84ce980832928d423484db686903.jpg';
+const imageAlt = 'Cat';
+
+const obj = {
+  name: "Google",
+  url: "https://www.google.com.ua/?hl=uk",
+};
+const colors = ["red", "green", "blue"];
+const num1 = 27;
+const num2 = 14;
+const sum = num1 + num2;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {name}
+        <br></br>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ласкаво просимо до нашого сайту!
+          <br>
+          </br>
+          <a href={obj.url}>{obj.name}</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Сума: {sum}</p>
+        <ul>
+          {colors.map((color, index) => (
+            <li key={index}>{color}</li>
+          ))}
+        </ul>
+        
       </header>
     </div>
   );
